d37c2e556e606b1cf8325a2f944f1a74bfdaa081
Naoya Fujimoto <soccer.ikimono.711@gmail.com>
2022-08-15 23:23:06 +0900

    20220815 23:22 header.phpの追加

---
+++ inc/header.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="jp">
+
+<head>
+    <!-- 文字コード -->
+    <meta charset="UTF-8">
+    <!-- IEバグ回避用 -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- 説明文 -->
+    <meta name="description" content="">
+    <!-- ビューポート -->
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <!-- OGP -->
+    <meta property="og:title" content="">
+    <meta property="og:type" content="website">
+    <meta property="og:description" content="">
+    <meta property="og:url" content="">
+    <meta property="og:site_name" content="">
+    <meta property="og:image" content="">
+    <!-- Twitter OGP -->
+    <meta name="twitter:card" content="summary_large_image">
+    <!-- ファビコン -->
+    <link rel="" href="">
+    <!-- Webフォント -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;700&family=Noto+Sans+JP:wght@400;500;700&display=swap" rel="stylesheet">
+    <!-- アイコンフォント -->
+    <link href="https://fonts.googleapis.com/css2?family=Material+Icons" rel="stylesheet">
+    <!-- CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/destyle.css@2.0.2/destyle.css">
+    <link rel="stylesheet" href="dist/css/style.css">
+    <link rel="stylesheet" href="https://use.typekit.net/qgw0lcp.css">
+</head>
+
+<body>
+
+<header class="l-header wrapper">
+    <div class="l-header__icon">NAOYA</div>
+    <nav class="l-header__nav">
+        <ul class="l-header__list">
+            <li class="l-header__link">
+                <ul class="l-header__item">
+                    <li class="l-header__item-english">MESSAGE</li>
+                    <li class="l-header__item-japanese">メッセージ</li>
+                </ul>
+            </li>
+            <li class="l-header__link">
+                <ul class="l-header__item">
+                    <li class="l-header__item-english">SKILL</li>
+                    <li class="l-header__item-japanese">スキル</li>
+                </ul>
+            </li>
+            <li class="l-header__link">
+                <ul class="l-header__item">
+                    <li class="l-header__item-english">WORKS</li>
+                    <li class="l-header__item-japanese">制作物</li>
+                </ul>
+            </li>
+            <li class="l-header__link">
+                <ul class="l-header__item">
+                    <li class="l-header__item-english">ABOUT</li>
+                    <li class="l-header__item-japanese">私について</li>
+                </ul>
+            </li>
+        </ul>
+    </nav>
+</header>
