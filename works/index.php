da1d0a3314a8e2a89954e06bf97232054359130e
Naoya Fujimoto <soccer.ikimono.711@gmail.com>
2022-08-17 21:53:36 +0900

    20220817 21:52 works.phpの追加

--- works/index.php
+++ works/index.php
@@ -1,3 +1,218 @@
 <?php include '../inc/header.php'; ?>
 
+<!-- MV -->
+<section class="p-works-mv l-wrapper">
+    <p class="p-works-mv__heading">WORKS</p>
+</section>
+
+<!-- ARCHIVE -->
+<section class="p-works-archive l-wrapper">
+    <div class="c-section-heading" id="message">
+        <p class="c-section-heading__japanese">アーカイブ</p>
+        <h2 class="c-section-heading__english">ARCHIVE</h2>
+    </div>
+
+    <div class="p-works-archive__container l-wrapper-sm">
+        <div class="p-works-archive__card">
+            <img src="../image/top-MK.png" alt="株式会社エムケーの画像" class="p-works-archive__card-image">
+            <p class="p-works-archive__card-heading">株式会社エムケー</p>
+            <div class="p-works-archive__card-button"></div>
+        </div>
+
+        <div class="p-works-archive__card">
+            <img src="../image/top-kitahama.png" alt="株式会社キタハマ総合法律事務所の画像" class="p-works-archive__card-image">
+            <p class="p-works-archive__card-heading">株式会社キタハマ総合法律事務所</p>
+            <div class="p-works-archive__card-button"></div>
+        </div>
+
+        <div class="p-works-archive__card">
+            <img src="../image/top-theplacenamba.png" alt="THE PLECA NAMBAの画像" class="p-works-archive__card-image">
+            <p class="p-works-archive__card-heading">THE PLECA NAMBA</p>
+            <div class="p-works-archive__card-button"></div>
+        </div>
+
+        <div class="p-works-archive__card">
+            <img src="../image/top-fujimetal.png" alt="株式会社ふじ芳金属加工の画像" class="p-works-archive__card-image">
+            <p class="p-works-archive__card-heading">THE PLECA NAMBA</p>
+            <div class="p-works-archive__card-button"></div>
+        </div>
+
+    </div>
+
+
+
+</section>
+
+
+<!-- CONTENT -->
+<section class="p-works-content l-wrapper">
+    <div class="c-section-heading" id="message">
+        <p class="c-section-heading__japanese">コンテンツ</p>
+        <h2 class="c-section-heading__english">CONTENT</h2>
+    </div>
+
+    <!-- 株式会社エムケー -->
+    <div class="p-works-content__box l-wrapper-sm">
+        <h3 class="p-works-content__heading p-works-content__heading--green">株式会社エムケー</h3>
+        <div class="p-works-content__thumbnail">
+            <img src="../image/works-MK.png" alt="株式会社エムケーの画像" class="p-works-content__image">
+        </div>
+
+        <div class="p-works-content__button">
+            <button class="p-works-content__button-link p-works-content__button-link--green">サイトはこちら</button>
+            <button class="p-works-content__button-link p-works-content__button-link--green">Gitを見る</button>
+        </div>
+
+        <div class="p-works-content__inner">
+            <div class="p-works-content__overview">
+                <h4 class="p-works-content__overview-heading p-works-content__overview-heading--green">概要</h4>
+                <p class="p-works-content__overview-text">Webサイトリニューアルに伴うWPテーマ作成を担当しました。</p>    
+            </div>
+
+            <div class="p-works-content__overview">
+                <h4 class="p-works-content__overview-heading p-works-content__overview-heading--green">使用ツール</h4>
+                <p class="p-works-content__overview-text">
+                    コーディング：PHP / Sass / JavaScript<br>
+                    CMS：WordPress<br>
+                    ライブラリ：jQuery<br>
+                    コード管理：Git
+                </p>    
+            </div>
+
+            <div class="p-works-content__overview">
+                <h4 class="p-works-content__overview-heading p-works-content__overview-heading--green">工数</h4>
+                <p class="p-works-content__overview-text">3週間</p>    
+            </div>
+
+            <div class="p-works-content__overview">
+                <h4 class="p-works-content__overview-heading p-works-content__overview-heading--green">工夫した点</h4>
+                <p class="p-works-content__overview-text">Webサイトリニューアルに伴うWPテーマ作成を担当しました。</p>    
+            </div>
+        </div>
+    </div>
+
+    <!-- キタハマ総合法律事務所 -->
+    <div class="p-works-content__box l-wrapper-sm">
+        <h3 class="p-works-content__heading p-works-content__heading--red">キタハマ総合法律事務所</h3>
+        <div class="p-works-content__thumbnail">
+            <img src="../image/works-kitahama.png" alt="キタハマ総合法律事務所の画像" class="p-works-content__image">
+        </div>
+
+        <div class="p-works-content__button">
+            <button class="p-works-content__button-link p-works-content__button-link--red">サイトはこちら</button>
+            <button class="p-works-content__button-link p-works-content__button-link--red">Gitを見る</button>
+        </div>
+
+        <div class="p-works-content__inner">
+            <div class="p-works-content__overview">
+                <h4 class="p-works-content__overview-heading p-works-content__overview-heading--red">概要</h4>
+                <p class="p-works-content__overview-text">Webサイトリニューアルに伴うWPテーマ作成を担当しました。</p>    
+            </div>
+
+            <div class="p-works-content__overview">
+                <h4 class="p-works-content__overview-heading p-works-content__overview-heading--red">使用ツール</h4>
+                <p class="p-works-content__overview-text">
+                    コーディング：PHP / Sass / JavaScript<br>
+                    CMS：WordPress<br>
+                    ライブラリ：jQuery<br>
+                    コード管理：Git
+                </p>    
+            </div>
+
+            <div class="p-works-content__overview">
+                <h4 class="p-works-content__overview-heading p-works-content__overview-heading--red">工数</h4>
+                <p class="p-works-content__overview-text">3週間</p>    
+            </div>
+
+            <div class="p-works-content__overview">
+                <h4 class="p-works-content__overview-heading p-works-content__overview-heading--red">工夫した点</h4>
+                <p class="p-works-content__overview-text">Webサイトリニューアルに伴うWPテーマ作成を担当しました。</p>    
+            </div>
+        </div>
+    </div>
+
+    <!-- THE PLACE NAMBA -->
+    <div class="p-works-content__box l-wrapper-sm">
+        <h3 class="p-works-content__heading p-works-content__heading--yellow">THE PLACE NAMBA</h3>
+        <div class="p-works-content__thumbnail">
+            <img src="../image/works-theplacenamba.png" alt="THE PLACE NAMBAの画像" class="p-works-content__image">
+        </div>
+
+        <div class="p-works-content__button">
+            <button class="p-works-content__button-link p-works-content__button-link--yellow">サイトはこちら</button>
+            <button class="p-works-content__button-link p-works-content__button-link--yellow">Gitを見る</button>
+        </div>
+
+        <div class="p-works-content__inner">
+            <div class="p-works-content__overview">
+                <h4 class="p-works-content__overview-heading p-works-content__overview-heading--yellow">概要</h4>
+                <p class="p-works-content__overview-text">Webサイトリニューアルに伴うWPテーマ作成を担当しました。</p>    
+            </div>
+
+            <div class="p-works-content__overview">
+                <h4 class="p-works-content__overview-heading p-works-content__overview-heading--yellow">使用ツール</h4>
+                <p class="p-works-content__overview-text">
+                    コーディング：PHP / Sass / JavaScript<br>
+                    CMS：WordPress<br>
+                    ライブラリ：jQuery<br>
+                    コード管理：Git
+                </p>    
+            </div>
+
+            <div class="p-works-content__overview">
+                <h4 class="p-works-content__overview-heading p-works-content__overview-heading--yellow">工数</h4>
+                <p class="p-works-content__overview-text">3週間</p>    
+            </div>
+
+            <div class="p-works-content__overview">
+                <h4 class="p-works-content__overview-heading p-works-content__overview-heading--yellow">工夫した点</h4>
+                <p class="p-works-content__overview-text">Webサイトリニューアルに伴うWPテーマ作成を担当しました。</p>    
+            </div>
+        </div>
+    </div>
+
+    <!-- 株式会社ふじ芳金属加工 -->
+    <div class="p-works-content__box l-wrapper-sm">
+        <h3 class="p-works-content__heading p-works-content__heading--blue">株式会社ふじ芳金属加工</h3>
+        <div class="p-works-content__thumbnail">
+            <img src="../image/works-fujimetal.png" alt="株式会社ふじ芳金属加工の画像" class="p-works-content__image">
+        </div>
+
+        <div class="p-works-content__button">
+            <button class="p-works-content__button-link p-works-content__button-link--blue">サイトはこちら</button>
+            <button class="p-works-content__button-link p-works-content__button-link--blue">Gitを見る</button>
+        </div>
+
+        <div class="p-works-content__inner">
+            <div class="p-works-content__overview">
+                <h4 class="p-works-content__overview-heading p-works-content__overview-heading--blue">概要</h4>
+                <p class="p-works-content__overview-text">Webサイトリニューアルに伴うWPテーマ作成を担当しました。</p>    
+            </div>
+
+            <div class="p-works-content__overview">
+                <h4 class="p-works-content__overview-heading p-works-content__overview-heading--blue">使用ツール</h4>
+                <p class="p-works-content__overview-text">
+                    コーディング：PHP / Sass / JavaScript<br>
+                    CMS：WordPress<br>
+                    ライブラリ：jQuery<br>
+                    コード管理：Git
+                </p>    
+            </div>
+
+            <div class="p-works-content__overview">
+                <h4 class="p-works-content__overview-heading p-works-content__overview-heading--blue">工数</h4>
+                <p class="p-works-content__overview-text">3週間</p>    
+            </div>
+
+            <div class="p-works-content__overview">
+                <h4 class="p-works-content__overview-heading p-works-content__overview-heading--blue">工夫した点</h4>
+                <p class="p-works-content__overview-text">Webサイトリニューアルに伴うWPテーマ作成を担当しました。</p>    
+            </div>
+        </div>
+    </div>
+
+</section>
+
+
+
 <?php include '../inc/footer.php'; ?>
