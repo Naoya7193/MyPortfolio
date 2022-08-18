221f844430446904daae49ad736d6181c191933e
Naoya Fujimoto <soccer.ikimono.711@gmail.com>
2022-08-18 16:16:00 +0900

    20220818 16:15 about.phpの追加と画像の追加

--- about/index.php
+++ about/index.php
@@ -1,3 +1,173 @@
 <?php include '../inc/header.php'; ?>
 
+<!-- MV -->
+<section class="p-about-mv l-wrapper">
+    <p class="p-about-mv__heading">
+        ABO<span class="l-text__blue-lighter">U</span>T
+    </p>
+</section>
+
+<!-- PROFILE -->
+<div class="p-about-profile l-wrapper">
+    <div class="p-about-profile__card">
+        <img src="./image/profile-image.png" alt="アイコン画像" class="p-about-profile__card-image">
+        <div class="p-about-profile__card-inner">
+            <h3 class="p-about-profile__card-heading">Naoya Fujimoto</h3>
+            <p class="p-about-profile__card-text">
+                奈良県生まれ奈良県育ちのエンジニア志望<br>
+                プログラムを書くことや新しい技術を勉強するのが好きです<br>
+                人と話したり話を聞いたりするのが好きです<br>
+                ものづくりと謎解きが好きで時々イラストも描きます<br>
+                よろしくお願いします！
+            </p>
+        </div>
+    </div>
+</div>
+
+<!-- MOTTO -->
+<section class="p-about-motto l-wrapper-sm">
+    <div class="c-section-heading">
+        <p class="c-section-heading__japanese">モットー</p>
+        <h2 class="c-section-heading__english">MOTTO</h2>
+    </div>
+    
+    <div class="p-about-motto__container">
+        <div class="p-about-motto__card">
+            <div class="p-about-motto__card-heading">
+                <div class="p-about-motto__card-number">01</div>
+                <h3 class="p-about-card__card-title">伝える</h3>
+            </div>
+            <img src="../image/about-telling.png" alt="伝えるイラスト" class="p-about-motto__card-image p-about-motto__card-image--red">
+            <p class="p-about-motto__card-text">前提知識を共有していない相手にも正しく意図を説明できます。また人前で自分の考えを話すことを得意としています。</p>
+        </div>
+
+        <div class="p-about-motto__card">
+            <div class="p-about-motto__card-heading">
+                <div class="p-about-motto__card-number">02</div>
+                <h3 class="p-about-card__card-title">聞く</h3>
+            </div>
+            <img src="../image/about-hearing.png" alt="聞くイラスト" class="p-about-motto__card-image p-about-motto__card-image--yellow">
+            <p class="p-about-motto__card-text">相手の意見を尊重し、考えの意図を理解することに努めます。また話し手が快く話せる姿勢を意識しています。</p>
+        </div>
+
+        <div class="p-about-motto__card">
+            <div class="p-about-motto__card-heading">
+                <div class="p-about-motto__card-number">03</div>
+                <h3 class="p-about-card__card-title">考える</h3>
+            </div>
+            <img src="../image/about-thinking.png" alt="考えるイラスト" class="p-about-motto__card-image p-about-motto__card-image--blue">
+            <p class="p-about-motto__card-text">お互いの考え方の違いを明確にした上で最善は何か検討し、多くの人が納得のいく方法の提案を目指します。</p>
+        </div>
+    </div>
+</section>
+
+<!-- CAREER -->
+<section class="p-about-career l-wrapper-sm">
+    <div class="c-section-heading">
+        <p class="c-section-heading__japanese">経歴</p>
+        <h2 class="c-section-heading__english">CAREER</h2>
+    </div>
+
+    <div class="p-about-career__container">
+        <div class="p-about-career__box">
+            <div class="p-about-career__year">1996</div>
+            <div class="p-about-career__inner">
+                <h3 class="p-about-career__box-heading">幼少期</h3>
+                <p class="p-about-career__box-text">
+                    幼少期は絵や工作を好むインドアな性格でした。<br>
+                    PCにも触れさせてもらい、ペイントツールを使って遊んでいました。
+                </p>
+            </div>
+        </div>
+
+        <div class="p-about-career__box">
+            <div class="p-about-career__year">2003</div>
+            <div class="p-about-career__inner">
+                <h3 class="p-about-career__box-heading">勉強と部活動の両立</h3>
+                <p class="p-about-career__box-text">小中学校では5年生から5年間吹奏楽部に所属。勉強にも楽しんで積極的に取り組んでいました。小学校ではトランペットを担当、中学校ではチューバを担当しました。</p>
+                
+                <div class="p-about-career__award">
+                    <h4 class="p-about-career__award-heading">受賞歴</h4>
+                    <p class="p-about-career__award-text">
+                        第26回全日本小学校バンドフェスティバル　金賞（小5）<br>
+                        第27回全日本小学校バンドフェスティバル　金賞（小6）<br>
+                        第58回全日本吹奏楽コンクール　銀賞（中2）<br>
+                        第59回全日本吹奏楽コンクール　金賞（中3）
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="p-about-career__box">
+            <div class="p-about-career__year">2012</div>
+            <div class="p-about-career__inner">
+                <h3 class="p-about-career__box-heading">奈良県立奈良高等学校に進学</h3>
+                <p class="p-about-career__box-text">高校でも吹奏楽部に所属し2年生で指揮者になりました。小中学校の時とは全く異なる環境の中で、指揮者として部活動全体について考えることの楽しさと難しさを経験しました。</p>
+            </div>
+        </div>
+
+        <div class="p-about-career__box">
+            <div class="p-about-career__year">2015</div>
+            <div class="p-about-career__inner">
+                <h3 class="p-about-career__box-heading">同志社大学文化情報学部に進学</h3>
+                <p class="p-about-career__box-text">
+                    ビッグデータの分析やデータサイエンスを専門に研究していました。<br>
+                    プログラミングも学習し始め、データ分析の分野で積極的に活用するようになりました。
+                </p>
+            </div>
+        </div>
+
+        <div class="p-about-career__box">
+            <div class="p-about-career__year">2017</div>
+            <div class="p-about-career__inner">
+                <h3 class="p-about-career__box-heading">1年間早稲田大学文化構想学部に交換留学</h3>
+                <p class="p-about-career__box-text">大学3年時、学びの幅を広げるために早稲田大学文化構想学部に交換留学しました。また機械学習やデータ分析のコンペティションにも取り組み、データ分析のコンペティションでは賞をいただきました。</p>
+                
+                <div class="p-about-career__award">
+                    <h4 class="p-about-career__award-heading">受賞歴</h4>
+                    <p class="p-about-career__award-text">第6回データビジネス創造コンテスト 入賞</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="p-about-career__box">
+            <div class="p-about-career__year">2018</div>
+            <div class="p-about-career__inner">
+                <h3 class="p-about-career__box-heading">同志社大学にて研究に専念</h3>
+                <p class="p-about-career__box-text">実データの分析を多く経験しました。J1リーグのトラッキングデータやECサイトのPOSデータ、テレビCMの視聴履歴データ等の分析を行いました。背景分析/課題設定/分析/施策の提案までのフローを経験しました。</p>
+                
+                <div class="p-about-career__award">
+                    <h4 class="p-about-career__award-heading">受賞歴</h4>
+                    <p class="p-about-career__award-text">第8回スポーツデータ解析コンペティション ポスター発表 最優秀賞</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="p-about-career__box">
+            <div class="p-about-career__year">2019</div>
+            <div class="p-about-career__inner">
+                <h3 class="p-about-career__box-heading">チームラボ株式会社に入社</h3>
+                <p class="p-about-career__box-text">Webアプリケーションの開発を担当しました。未経験の分野で上手くいかないことも多かったですが、Webの分野の面白さや開発職の面白さ、モダンなチーム開発の環境を経験できました。</p>
+            </div>
+        </div>
+
+        <div class="p-about-career__box">
+            <div class="p-about-career__year">2021</div>
+            <div class="p-about-career__inner">
+                <h3 class="p-about-career__box-heading">Webスクールへの通学を開始</h3>
+                <p class="p-about-career__box-text">Webの開発に必要な基盤知識を学習するために入校しました。現在は全てのカリキュラムを修了し、転職活動と並行してWebスクールのメンターとしてコーディングのフィードバックをお手伝いしています。</p>
+            </div>
+        </div>
+
+        <div class="p-about-career__box p-about-career__box--yellow">
+            <div class="p-about-career__year">VISION</div>
+            <div class="p-about-career__inner">
+                <h3 class="p-about-career__box-heading">Webエンジニアを目指して勉強中！</h3>
+                <p class="p-about-career__box-text">Webの開発に必要な基盤知識を学習するために入校しました。現在は全てのカリキュラムを修了し、転職活動と並行してWebスクールのメンターとしてコーディングのフィードバックをお手伝いしています。</p>
+            </div>
+        </div>
+    </div>
+
+</section>
+
 <?php include '../inc/footer.php'; ?>
