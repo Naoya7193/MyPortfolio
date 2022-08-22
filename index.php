7fff230cb38a046ba88f29404cced8308e6101bf
Naoya Fujimoto <soccer.ikimono.711@gmail.com>
2022-08-22 17:38:44 +0900

    20220822 17:38 index.phpのSPレスポンシブ追加

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
         <h2 class="c-section-heading__english">MESSAGE</h2>
     </div>
 
-    <p class="p-index-message__copy">「ワクワク」する気持ちを大切にしています</p>
+    <p class="p-index-message__copy"><span>「ワクワク」する気持ちを</span><span>大切にしています</span></p>
     <p class="p-index-message__text">
         僕は「ものづくりを通して多くの人にワクワクする体験を届けたい」と考えています。<br>
         そのためには多くの人にワクワクしてもらえるプロダクトを世に出す必要があります。<br>
@@ -33,30 +33,26 @@
     <div class="p-index-skill__container">
         <div class="p-index-skill__card">
             <img src="./image/top-webdevelopment.png" alt="Web開発の画像" class="p-index-skill__image">
-            <div class="p-index-skill__card-inner">
-                <h3 class="p-index-skill__card-heading">Web開発</h3>
-                <p class="p-index-skill__card-text">
-                    Web制作とWebアプリケーション開発の経験があります。Web制作では基本的なコーディングからWPのテーマ開発まで実装可能で、Webアプリケーション開発ではRuby on Railsを用いたアプリケーションの開発が可能です。またコードはGit上で管理し、環境構築にはDockerを使うこともあります。<br>
-                    Web制作歴：8ヶ月　Webアプリケーション開発歴：6ヶ月
-                </p>
-                <div class="p-index-skill__tool">
-                    <p class="p-index-skill__tool-heading">関連スキル</p>
-                    <p class="p-index-skill__tool-text">HTML/CSS/JavaScript/PHP/Sass/WordPress/Ruby on Rails/Git/Docker</p>
-                </div>
+            <h3 class="p-index-skill__card-heading">Web開発</h3>
+            <p class="p-index-skill__card-text">
+                Web制作とWebアプリケーション開発の経験があります。Web制作では基本的なコーディングからWPのテーマ開発まで実装可能で、Webアプリケーション開発ではRuby on Railsを用いたアプリケーションの開発が可能です。またコードはGit上で管理し、環境構築にはDockerを使うこともあります。<br>
+                Web制作歴：8ヶ月　Webアプリケーション開発歴：6ヶ月
+            </p>
+            <div class="p-index-skill__tool">
+                <p class="p-index-skill__tool-heading">関連スキル</p>
+                <p class="p-index-skill__tool-text">HTML/CSS/JavaScript/PHP/Sass/WordPress/Ruby on Rails/Git/Docker</p>
             </div>
         </div>
 
         <div class="p-index-skill__card">
             <img src="./image/top-dataanalytics.png" alt="データ解析の画像" class="p-index-skill__image">
-            <div class="p-index-skill__card-inner">
-                <h3 class="p-index-skill__card-heading">データ解析</h3>
-                <p class="p-index-skill__card-text">
-                    大学4年間で専門的に学習し、統計学や多変量解析の知識、RやPythonを用いた実データ解析の経験があります。またビッグデータ解析におけるデータハンドリングではSQLite等のデータベースを利用します。統計検定2級を取得、受賞歴に「第6回データビジネス創造コンテスト 入賞」「第8回スポーツデータ解析コンペティション ポスター発表 最優秀賞」。
-                </p>
-                <div class="p-index-skill__tool">
-                    <p class="p-index-skill__tool-heading">関連スキル</p>
-                    <p class="p-index-skill__tool-text">R/Python/SQLite</p>
-                </div>
+            <h3 class="p-index-skill__card-heading">データ解析</h3>
+            <p class="p-index-skill__card-text">
+                大学4年間で専門的に学習し、統計学や多変量解析の知識、RやPythonを用いた実データ解析の経験があります。またビッグデータ解析におけるデータハンドリングではSQLite等のデータベースを利用します。統計検定2級を取得、受賞歴に「第6回データビジネス創造コンテスト 入賞」「第8回スポーツデータ解析コンペティション ポスター発表 最優秀賞」。
+            </p>
+            <div class="p-index-skill__tool">
+                <p class="p-index-skill__tool-heading">関連スキル</p>
+                <p class="p-index-skill__tool-text">R/Python/SQLite</p>
             </div>
         </div>
     </div>
